c587364ca986a3c25eb7f5254d6afc32ee8e118f
HamzaAit <hait74144@gmail.com>
2023-09-24 14:15:14 +0200

    Fixed

--- src/public/func/functions.js
+++ src/public/func/functions.js
@@ -27,18 +27,18 @@ window.addEventListener("scroll", function() {
     }
 });
 
-// JavaScript (script.js)
 document.addEventListener("DOMContentLoaded", function() {
     const recentBlogsElement = document.querySelector(".recent-blogs");
-    const scrollPositionToShowRecentBlogs = window.innerHeight / 2; // Aparece cuando la mitad de la sección es visible
     
-    window.addEventListener("scroll", function() {
-        const rect = recentBlogsElement.getBoundingClientRect();
-        if (rect.top <= scrollPositionToShowRecentBlogs) {
-            recentBlogsElement.classList.add("show");
-        }
-    });
+    function showRecentBlogs() {
+        recentBlogsElement.classList.add("show");
+    }
+
+    // Llamar a la función después de un breve retraso (por ejemplo, 1000 milisegundos = 1 segundo)
+    setTimeout(showRecentBlogs, 200); // Ajusta el tiempo según tus preferencias
 });
+
+
 
 document.addEventListener("DOMContentLoaded", function() {
     const recentBlogsElement = document.querySelector(".about-us");
